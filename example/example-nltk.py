c4f96134499fbc9e5747ce33b650861b3761a40c
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2024-06-25 23:51:47 +0900

    implement data augmentation that also include original data entry

--- example/example-nltk.py
+++ example/example-nltk.py
@@ -10,8 +10,8 @@ stemmer = PorterStemmer()
 stopword_list = set(stopwords.words('english'))
 
 # sentence = "It is fine today. Today's weather is fine. Tom likes rainy weather but Sue not. Jane likes dogs and Mike likes cat."
-# sentence = "All work and no play makes jack dull boy."
-sentence = "Yucaipa owned Dominick 's before selling the chain to Safeway in 1998 for $ 2.5 billion ."
+sentence = "All work and no play makes jack dull boy."
+# sentence = "Yucaipa owned Dominick 's before selling the chain to Safeway in 1998 for $ 2.5 billion ."
 
 tokenized_words = word_tokenize(sentence)
 stemmed_words = []
