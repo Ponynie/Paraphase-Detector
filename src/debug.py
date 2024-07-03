0a6d84cb53d215c87b9a4474bdb8f46a9b219c88
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2024-07-03 16:30:52 +0900

    + original data count to count data

--- src/debug.py
+++ src/debug.py
@@ -1,22 +1,28 @@
 
 
-augmented_datasets = [
-    ('original', 'O'), #! Do not remove this (Original data needed to cache the augmented data)
-    ('augmented_1', 'A1'), # Synonym Substitution
-    ('augmented_2', 'A2'), # Word Paraphrase
-    ('augmented_3', 'A3'), # BackTranslation
-    ('augmented_4', 'A4'), # Random Word Deletion
-    ('augmented_5', 'A5'), # Subject Object Switch
-    ('full_augmented', 'FA') # All augmentations
+augmentation_methods = [
+    ('original', 'O', 'Original'),
+    ('augmented_1', 'A1', 'Synonym Substitution'),
+    ('augmented_2', 'A2', 'Word Paraphrase'),
+    ('augmented_3', 'A3', 'Backtranslation'),
+    ('augmented_4', 'A4', 'Random Word deletion'),
+    ('augmented_5', 'A5', 'Subject Object switch')
 ]
 import os
 
+s = 0
+a = 0
 path = 'data/MRPC_data'
-for folder, prefix in augmented_datasets:
+for folder, prefix, name in augmentation_methods:
     path_folder = os.path.join(path, folder)
     path_file = os.path.join(path_folder, f'{prefix}_MRPC_train.txt')
     with open(path_file, 'r') as file:
-        print(f"File: {path_file}")
+        print(f"Augmentation: {name}")
         f = file.readlines()
         length = len(f) - 1
-        print(f"Number of lines: {length}")
+        a += length
+        print(f"Number of data: {length + s}")
+        if prefix == 'O':
+            s = length
+            
+print(f"Full Augmentation: {a}")
